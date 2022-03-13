30d45be28e6b42428319cb6f3ce77bedcbc0aab9
Kangbeen Ko <sweetrainforyou@gmail.com>
2022-03-13 16:49:09 +0900

    feat(front-end_streamlit.py): add recommendation features

--- front-end_streamlit.py
+++ front-end_streamlit.py
@@ -1,24 +1,33 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import numpy as np
 
 movies_pickle = pickle.load(open('pickle/movies_dict.pkl', 'rb'))
-movies_list = pd.DataFrame(movies_pickle)['title'].values
-genre_sim_sorted_ind = pickle.load(open('pickle/genre_sim_sorted_ind.pkl', 'rb'))
+movies = pd.DataFrame(movies_pickle)
+movies_list = movies['title']
+genre_sim = pickle.load(open('pickle/genre_sim.pkl', 'rb'))
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index.values
+    distances = genre_sim[movie_index]
+    recommend_list = distances.argsort()[:, ::-1][0][1:11]
+    recommended_movies = []
+
+    for i in recommend_list:
+        recommended_movies.append(movies_list.iloc[i])
+    return recommended_movies
+
 st.title('MovieRecSys Demo')
 
 selected_movie_name = st.selectbox(
-    'How would you like to be contacted?',
-    movies_list['title'].values
-)
-
-# if st.button('Recommend'):
-#     df = recommend(selected_movie_name)
-#     st.write(f"Recommendation based on genres of '{selected_movie_name}'")
-#     for i in df['title'].values:
-#         st.write(i)
-    
-option = st.selectbox(
     'Search the movie you likes.',
     movies_list
 )
+
+if st.button('Recommend'):
+    df = recommend(selected_movie_name)
+    st.write(f"Recommendation based on genres of '{selected_movie_name}'")
+    for i in df:
+        st.write(i)
